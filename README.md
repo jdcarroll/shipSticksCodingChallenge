43f2d599c11583c187747bf407a14c93f274e514
jdcarroll <jdcarroll22@gmail.com>
2016-05-19 11:44:18 -0400

    final commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ship Sticks Coding challenge
+
+## Installation
+
+I created an install script that kicks everything off.
+
+Just make sure that you have node and mongo installed
+
+Step1
+
+```
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,4 +1,35 @@
-#!bin/bash
+#!/bin/bash
 # Code Challenge install script
 
+echo 'Importing data into mongodb'
 mongoimport --jsonArray --db ShipSticks --collection products --file products.json
+
+command -v bower >/dev/null 2>&1 || { 
+
+	echo "Installing bower " >&2; 
+
+	npm install bower
+
+}
+command -v gulp >/dev/null 2>&1 || { 
+
+	echo "Installing gulp " >&2; 
+
+	npm install gulp
+
+}
+echo ''
+echo 'installing node dependancys'
+echo ''
+npm install
+
+echo ''
+echo 'installing bower dependancys'
+echo ''
+bower install 
+
+gulp &
+
+echo 'Launching App: at localhost:3000'
+
+open "http://localhost:3000"
